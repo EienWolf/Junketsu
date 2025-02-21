a2a90a27dd7a54a372b320ad021b1ccde58d1c9a
EienWolf <miguel.padillaabrego@gmail.com>
2025-02-20 21:47:10 -0600

    primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { NavComponent } from "./components/nav/nav.component";
 import { DOCUMENT } from '@angular/common';
+import { PrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,20 @@ import { DOCUMENT } from '@angular/common';
 })
 export class AppComponent {
   DEFAULT_LANGUAGE:string = 'es_MX';
-  constructor(@Inject(DOCUMENT) private document: Document, private translate: TranslateService) {
+  constructor(@Inject(DOCUMENT) private document: Document, private translate: TranslateService
+    , private primeng: PrimeNG) {
     this.translate.setDefaultLang(this.DEFAULT_LANGUAGE);
     this.detectLanguage();
+    this.primeng.theme.set({
+      preset: Aura,
+          options: {
+            darkModeSelector: '[data-theme="dark"]',
+              cssLayer: {
+                  name: 'primeng',
+                  order: 'tailwind-base, primeng,  tailwind-utilities'
+              }
+          }
+      });
   }
 
   changeLanguage(lang: string) {
