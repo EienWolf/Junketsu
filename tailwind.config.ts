6addd55d6d9a4937f71ca45a9df33f0072e12bb8
EienWolf <miguel.padillaabrego@gmail.com>
2025-03-09 13:16:05 -0600

    xs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
 import type { Config } from "tailwindcss";
-
+module.exports = {
+    theme: {
+      screens: {
+        'xs': '448px'
+    }
+  }
+}
 export default {
     plugins: [require('tailwindcss-primeui')],
 } satisfies Config;
