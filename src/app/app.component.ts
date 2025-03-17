3ec719ec9c935747f8aba553c974cf8b21ad341d
Eien Wolf <miguel.padillaabrego@gmail.com>
2025-03-16 22:23:50 -0600

    test of prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { NavComponent } from "./components/nav/nav.component";
+import { NavComponent } from './components/nav/nav.component';
 import { DOCUMENT } from '@angular/common';
 import { PrimeNG } from 'primeng/config';
-import  Material  from '@primeng/themes/material';
+import Material from '@primeng/themes/material';
 import { SupabaseService } from './services/supabase.service';
 import { MyPreset } from '../mytheme';
 
@@ -12,36 +12,34 @@ import { MyPreset } from '../mytheme';
   selector: 'app-root',
   imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit{
-  DEFAULT_LANGUAGE:string = 'es_MX';
-  session:any;
+export class AppComponent implements OnInit {
+  DEFAULT_LANGUAGE: string = 'es_MX';
+  session: any;
 
-
-  
-
-  constructor(@Inject(DOCUMENT) private document: Document, private translate: TranslateService
-    , private primeng: PrimeNG, private readonly supabase: SupabaseService) {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private translate: TranslateService,
+    private primeng: PrimeNG,
+    private readonly supabase: SupabaseService,
+  ) {
     this.session = this.supabase.session;
     this.translate.setDefaultLang(this.DEFAULT_LANGUAGE);
     this.primeng.theme.set({
       preset: MyPreset,
-          options: {
-            darkModeSelector: '[data-theme="dark"]',
-              cssLayer: {
-                  name: 'primeng',
-                  order: 'base, primeng, tailwind-utilities'
-              }
-          },
-          
-            
-      });
+      options: {
+        darkModeSelector: '[data-theme="dark"]',
+        cssLayer: {
+          name: 'primeng',
+          order: 'base, primeng, tailwind-utilities',
+        },
+      },
+    });
   }
 
   ngOnInit() {
-    this.supabase.authChanges((_, session) => (this.session = session))
+    this.supabase.authChanges((_, session) => (this.session = session));
   }
   title = 'junketsu';
 }
-
